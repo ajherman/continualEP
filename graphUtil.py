310e71a792b333619e21636a7728edf66ca707a8
Ari Herman <ari.j.herman@gmail.com>
2023-09-20 16:55:30 -0700

    wip

--- graphUtil.py
+++ graphUtil.py
@@ -26,25 +26,15 @@ def csv2array(directory):
     return train_error, test_error
 
 
-fig, ax = plt.subplots(2,5)
+fig, ax = plt.subplots(2,5,figsize=(10,4))
+labels=['spiking','nonspiking']
 
 for i in range(10):
     spiking_dir = 'cepalt_spiking_'+str(i)
     nonspiking_dir = 'cepalt_nonspiking_'+str(i)
     spiking_train_error, spiking_test_error = csv2array(spiking_dir)
-    nonspiking_train_error,non_spiking_test_error = csv2array(nonspiking_dir)
-    ax.plot[i//5,i%5](spiking_test_error)
-    ax.plot[i//5,i%5](nonspiking_test_error)
-fig.savefig('cepalt_error.png')
-
-#
-# train_error1,test_error1 = csv2array('cepalt_1')
-# train_error2,test_error2 = csv2array('cepalt_2')
-# train_error3,test_error3 = csv2array('cepalt_3')
-# fig, ax = plt.subplots(3)
-# ax.plot[0](train_error1)
-# ax.plot[1](train_error2)
-# ax.plot[2](train_error3)
-
-fig.savefig(args.directory+'/error.png')
-# print(errors)
+    nonspiking_train_error,nonspiking_test_error = csv2array(nonspiking_dir)
+    ax[i//5,i%5].plot(spiking_test_error)
+    ax[i//5,i%5].plot(nonspiking_test_error)
+fig.legend(labels, loc='lower right', ncol=len(labels), bbox_transform=fig.transFigure)
+fig.savefig('cepalt_error.png',bbox_inches="tight")
