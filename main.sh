ec80306a3e62a405e9a02358cd70a928d1534224
Ari Herman <ari.j.herman@gmail.com>
2023-09-26 12:11:25 -0600

    wip

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=main
-#SBATCH--time 10:00:00
+#SBATCH --time 10:00:00
 #SBATCH -N 4
 #SBATCH -p shared-gpu
-module load miniconda3
-source activate /vast/home/ajherman/miniconda3/envs/pytorch
+#module load miniconda3
+#source activate /vast/home/ajherman/miniconda3/envs/pytorch
 
 # Plot 1 (Variable N, compensating dt)
 i=0
