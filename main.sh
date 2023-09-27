6fda6084842f2b3e0a3d554a9aee259086dd5fd3
Ari Herman <ari.j.herman@gmail.com>
2023-09-27 15:47:41 -0700

    wip

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=main
 #SBATCH --time 10:00:00
-#SBATCH -N 6
+#SBATCH -N 8
 #SBATCH -p shared-gpu
 #module load miniconda3
 #source activate /vast/home/ajherman/miniconda3/envs/pytorch
@@ -90,6 +90,31 @@
 # ########################################################################################################################################################################################
 
 
+# Compare spiking/nonspiking cepalt/skewsym
+######################################################################################################################################################
+T=40
+Kmax=15
+beta=0.5
+step=1.0
+max_fr=1.0
+spiking_skewsym_dir=spiking_skewsym
+nonspiking_skewsym_dir=nonspiking_skewsym
+spiking_cepalt_dir=spiking_cepalt
+nonspiking_cepalt_dir=nonspiking_cepalt
+
+mkdir -p $spiking_skewsym_dir
+mkdir -p $nonspiking_skewsym_dir
+mkdir -p $spiking_cepalt_dir
+mkdir -p $nonspiking_cepalt_dir
+
+srun -N 1 -n 1 -c 6 -o "$spiking_skewsym_dir".out --open-mode=append ./main_wrapper.sh --directory $spiking_skewsym_dir --spike-height 1.0 --spiking --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule skewsym &
+srun -N 1 -n 1 -c 6 -o "$nonspiking_skewsym_dir".out --open-mode=append ./main_wrapper.sh --directory $nonspiking_skewsym_dir --spike-height 1.0 --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule skewsym &
+srun -N 1 -n 1 -c 6 -o "$spiking_cepalt_dir".out --open-mode=append ./main_wrapper.sh --directory $spiking_cepalt_dir --spike-height 1.0 --spiking --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule cepalt &
+srun -N 1 -n 1 -c 6 -o "$nonspiking_cepalt_dir".out --open-mode=append ./main_wrapper.sh --directory $nonspiking_cepalt_dir --spike-height 1.0 --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule cepalt &
+######################################################################################################################################################
+
+
+
 # Spiking and nonspiking cepalt for various levels of discretization (fixed max_fr)
 #######################################################################################################################################################
 i=0
@@ -135,30 +160,6 @@ for Kmax in {27,30,33,36}
 		srun -N 1 -n 1 -c 12 -o "$nonspiking_dir".out --open-mode=append ./main_wrapper.sh --directory $nonspiking_dir --max-fr $max_fr --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 30 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule $update_rule  &
 		i=$((i+1))
 	done
-######################################################################################################################################################
-
-
-# Compare spiking/nonspiking cepalt/skewsym
-######################################################################################################################################################
-T=40
-Kmax=15
-beta=0.5
-step=1.0
-max_fr=1.0
-spiking_skewsym_dir=spiking_skewsym
-nonspiking_skewsym_dir=nonspiking_skewsym
-spiking_cepalt_dir=spiking_cepalt
-nonspiking_cepalt_dir=nonspiking_cepalt
-
-mkdir -p $spiking_skewsym_dir
-mkdir -p $nonspiking_skewsym_dir
-mkdir -p $spiking_cepalt_dir
-mkdir -p $nonspiking_cepalt_dir
-
-srun -N 1 -n 1 -c 4 -o "$spiking_skewsym_dir".out --open-mode=append ./main_wrapper.sh --directory $spiking_skewsym_dir --spike-height 1.0 --spiking --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule skewsym &
-srun -N 1 -n 1 -c 4 -o "$nonspiking_skewsym_dir".out --open-mode=append ./main_wrapper.sh --directory $nonspiking_skewsym_dir --spike-height 1.0 --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule skewsym &
-srun -N 1 -n 1 -c 4 -o "$spiking_cepalt_dir".out --open-mode=append ./main_wrapper.sh --directory $spiking_cepalt_dir --spike-height 1.0 --spiking --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule cepalt &
-srun -N 1 -n 1 -c 4 -o "$nonspiking_cepalt_dir".out --open-mode=append ./main_wrapper.sh --directory $nonspiking_cepalt_dir --spike-height 1.0 --action train --activation-function hardsigm --size_tab 10 256 784 --lr_tab 0.0028 0.0056 --epochs 50 --T $T --Kmax $Kmax --beta $beta --cep --learning-rule stdp --update-rule cepalt &
 ######################################################################################################################################################
 
 
