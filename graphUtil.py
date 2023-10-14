b5efdcbee0e6de4e239eb289057a341185d5c0c7
Ari Herman <ari.j.herman@gmail.com>
2023-10-14 14:18:12 -0700

    wip

--- graphUtil.py
+++ graphUtil.py
@@ -82,16 +82,13 @@ def dir_name2(learning_rule,N1,N2,n_dynamic,beta,batch_size):
 # fig.savefig('test.png',bbox_inches="tight")
 
 # This is what I was using
-n_col,n_row=2,2
-# batch_size=200
-N1=100
-# N2=30
-batch_size=200
+n_col,n_row=4,4
+
 fig, ax = plt.subplots(n_row,n_col,figsize=(40,40))
 layers=2
-rules=['stdp_slow','stdp_med','stdp_fast','nonspiking_stdp_slow','nonspiking_stdp_med','nonspiking_stdp_fast','nonspiking_skewsym','nonspiking_cep']
-# rules=['nonspiking_stdp_slow']#,'nonspiking_skewsym']
-for idx,step in enumerate([0.5,0.2,0.1,0.05]):
+# rules=['stdp_slow','stdp_med','stdp_fast','nonspiking_stdp_slow','nonspiking_stdp_med','nonspiking_stdp_fast','nonspiking_skewsym','nonspiking_cep']
+rules=['nonspiking_cep','nonspiking_skewsym']
+for idx in range(16):
     # for idx2,tau_trace in enumerate([0.006, 0.06, 0.6]):
     color = iter(colormap(np.linspace(0,1,12)))
     for rule in rules:
@@ -105,16 +102,16 @@ for idx,step in enumerate([0.5,0.2,0.1,0.05]):
             #     print("lr",learning_rule)
             #     print("dyn",n_dynamic)
             #     print("")
-            ax[idx//2,idx%2].plot(error[1],c=next(color),linewidth=1)
+            ax[idx//4,idx%4].plot(error[1],c=next(color),linewidth=1)
         except:
-            ax[idx//2,idx%2].plot([0],c=next(color))
+            ax[idx//4,idx%4].plot([0],c=next(color))
             print(directory_name)
-    ax[idx//2,idx%2].set_xlabel('Epoch')
-    ax[idx//2,idx%2].set_ylabel('Test error rate (%)')
-    ax[idx//2,idx%2].set_xlim([0,100])
-    ax[idx//2,idx%2].set_ylim([0,20])
-    ax[idx//2,idx%2].grid(axis='y')
-    ax[idx//2,idx%2].set_title('step = '+str(step))
+    ax[idx//4,idx%4].set_xlabel('Epoch')
+    ax[idx//4,idx%4].set_ylabel('Test error rate (%)')
+    ax[idx//4,idx%4].set_xlim([0,100])
+    ax[idx//4,idx%4].set_ylim([0,20])
+    ax[idx//4,idx%4].grid(axis='y')
+    # ax[idx//2,idx%2].set_title('step = '+str(step))
 fig.suptitle('Test Error (%)\n'+r'$T_1=8, T_2=3, \beta=0.2$'+'\n'+'stdp: slow =1.0, med=0.5, fast=0.05')
 fig.legend(rules, loc='lower right', ncol=len(rules), bbox_transform=fig.transFigure)
 fig.savefig('nonspiking_dynamics_'+str(layers)+'layer.png',bbox_inches="tight")
