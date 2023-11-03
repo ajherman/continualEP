aee86bb708fc2cfc228d043caa0082ab1f3ff84a
Ari Herman <ari.j.herman@gmail.com>
2023-11-03 12:47:38 -0600

    wip

--- test.py
+++ test.py
@@ -7,6 +7,59 @@ def f(omega,epoch):
         x=pkl.load(f)
     return x
 
-x=f(1024,10)
-y=x['phase1']['s']
-print(len(y))
+def g(x,phase,layer,batch,node):
+    y=x['phase1']['s']
+    w=x['phase1']['spike']
+#layer=1
+#batch=0
+#node=73
+    vals = np.array([z[layer][batch,node] for z in y])
+    spikes = np.array([z[layer][batch,node] for z in w])
+    if phase == 2:
+        pass
+    return vals,spikes
+
+node1 = (1,7)
+node2 = (0,7)
+
+data_dict=f(16,0)
+
+def getInfo(data_dict,node1,node2):
+    layer1,pos1 = node1
+    layer2,pos2 = node2
+    w_idx = 2*layer2
+    if layer1<layer2:
+        w_idx-=1
+    phase2_dict = data_dict['phase2']
+    s,spike,w = phase2_dict['s'],phase2_dict['spike'],phase2_dict['w']
+    s1_arr = np.array([z[layer1][0,pos1] for z in s])
+    s2_arr = np.array([z[layer2][0,pos2] for z in s])
+    spike1_arr = np.array([z[layer1][0,pos1] for z in spike])
+    spike2_arr = np.array([z[layer2][0,pos2] for z in spike])
+    w_arr = np.array([z[w_idx][pos2,pos1] for z in w])
+    #print(np.shape(w[0][w_idx]))
+    return s1_arr,s2_arr,spike1_arr,spike2_arr,w_arr
+
+for pos1 in range(200):
+    for pos2 in range(10):
+        node1=(1,pos1)
+        node2=(0,pos2)
+        s1_arr,s2_arr,spike1_arr,spike2_arr,w_arr = getInfo(data_dict,node1,node2)
+        if np.std(w_arr)>5e-5:
+            print("\n\n")
+            print(w_arr)
+
+def plot(data_dict,node1,node2):
+    s1_arr,s2_arr,spike1_arr,spike2_arr,w_arr=getInfo(data_dict,node1,node2)
+    
+#print(s1_arr)
+#print(s2_arr)
+#print(spike1_arr)
+#print(spike2_arr)
+#print(w_arr)
+
+#vals,spikes=g(x,1,1,0,73)
+#print(vals)
+#print(spikes)
+#print(vals-spikes)
+#print(np.shape(y[0][1]))
