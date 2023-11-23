9c74d47cd499e57eb53c5eea583cd1749e0477e4
Ari Herman <ari.j.herman@gmail.com>
2023-11-22 17:47:54 -0700

    wip

--- expand.sh
+++ expand.sh
@@ -23,7 +23,7 @@ tau_dynamic=0.2
 # omega=1
 for M in {1,2,4,7}
 do
-for omega in {1,4,15,50,2048}
+for omega in {1,4,15,50,1e24}
 do
 # cep_dir=compare_cep_M_"$M"_omega_"$omega"
 skewsym_dir=normal_skewsym_M_"$M"_omega_"$omega"
