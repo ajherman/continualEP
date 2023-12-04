c0d93acdc66d901fe537b5faecbf6dd423fc7634
Ari Herman <ari.j.herman@gmail.com>
2023-12-03 23:57:59 -0700

    wip

--- main.py
+++ main.py
@@ -439,7 +439,7 @@ if __name__ == '__main__':
         while net.current_epoch<args.epochs:
             epoch=net.current_epoch
             save_interval=50
-            error_train,data = train(net, train_loader, epoch, args.learning_rule,save_interval)
+            error_train,data = train(net, train_loader, epoch, args.learning_rule,save_interval,args.directory)
 
             error_train_tab.append(error_train)
 
