3f880c31d9d267befdc3d3d8c6565a38fdc566ec
Ari Herman <ari.j.herman@gmail.com>
2023-12-15 09:52:59 -0800

    wip

--- expand.sh
+++ expand.sh
@@ -47,22 +47,22 @@ beta=0.2
 T1=8
 T2=3
 hidden_size=256
-step=0.02 # Keep fixed
+step=0.05 #0.02 # Keep fixed
 batch_size=20
 tau_dynamic=0.2
-for M in {15,}
+for M in {8,16}
 do
 for omega in {1,}
 do
 skewsym_dir=binom_skewsym_M_"$M"_omega_"$omega"
 stdp1_dir=binom_stdp1_M_"$M"_omega_"$omega"
-stdp2_dir=binom_stdp2_M_"$M"_omega_"$omega"
-stdp3_dir=binom_stdp3_M_"$M"_omega_"$omega"
+# stdp2_dir=binom_stdp2_M_"$M"_omega_"$omega"
+# stdp3_dir=binom_stdp3_M_"$M"_omega_"$omega"
 
 mkdir -p $skewsym_dir
 mkdir -p $stdp1_dir
-mkdir -p $stdp2_dir
-mkdir -p $stdp3_dir
+# mkdir -p $stdp2_dir
+# mkdir -p $stdp3_dir
 
 
 srun -N 1 -n 1 -c $cores -o "$skewsym_dir".out --open-mode=append ./main_wrapper.sh --M $M --spiking --load --use-time-variables --directory $skewsym_dir --omega $omega --step $step --spike-method binomial --tau-dynamic $tau_dynamic --action train --batch-size $batch_size --activation-function hardsigm --size_tab 10 $hidden_size 784 --lr_tab 0.0028 0.0056 --epochs $epochs --T1 $T1 --T2 $T2 --beta $beta --cep --learning-rule stdp --update-rule skewsym &
