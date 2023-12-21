57de074483b25b290a391bbd5f07e7094ea35eb1
Ari Herman <ari.j.herman@gmail.com>
2023-12-20 23:37:30 -0800

    wip

--- netClasses.py
+++ netClasses.py
@@ -139,7 +139,9 @@ class SNN(nn.Module):
 
             elif self.update_rule == 'nonspikingstdp': # Still need to modify this to match above
                 for i in range(self.ns+1):
-                    trace[i] = self.trace_decay*(trace[i]+self.activation(s[i]))
+                    # trace[i] = self.trace_decay*(trace[i]+self.activation(s[i]))
+                    trace[i] = self.trace_decay*trace[i]+self.activation(s[i])*(1-self.trace_decay)**2
+                    # trace[i] = self.trace_decay*trace[i]+self.activation(s[i])*(1-self.trace_decay)
 
         # Update s
         if self.no_clamp:
